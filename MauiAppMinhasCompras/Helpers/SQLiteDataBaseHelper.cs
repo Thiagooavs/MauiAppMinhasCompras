e2dd7dee1fea632595ee4be950dcfbc977573c3d
Usuario <Usuario@CPD03-M11>
2024-11-04 15:08:55 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using MauiAppMinhasCompras.Modells;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    internal class SQLiteDataBaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDataBaseHelper (string path)
+        {
+            _conn = new SQLiteAsyncConnection (path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produtp SET Descricao=?, Quantidade=?, Preco=?, WHERE id=?";
+
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.id);
+        }
+    }
+}
---
+++ MauiAppMinhasCompras/Modells/Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Modells
+{
+    internal class Produto
+    {
+        string _descricao;
+
+        [PrimaryKey, AutoIncrement]
+
+        public int id {  get; set; }
+
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception(
+                        "Por favor, preencha a descrção");
+                }
+                _descricao = value;
+            }
+        }
+
+        public double Quantidade { get; set; }
+        public double Preco {  get; set; } 
+        public double Total { get => Quantidade * Preco; }
+           
+
+    }
+}
