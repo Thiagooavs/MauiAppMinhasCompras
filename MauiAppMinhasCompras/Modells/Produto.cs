a8c5d9314c2eeff60c1424bd28a2a30fbcabae89
Thiago A. V. de Souza <140747252+Thiagooavs@users.noreply.github.com>
2024-11-29 15:06:09 -0300

    Update Produto.cs

--- MauiAppMinhasCompras/Modells/Produto.cs
+++ MauiAppMinhasCompras/Modells/Produto.cs
@@ -4,29 +4,15 @@ namespace MauiAppMinhasCompras.Modells
 {
     public class Produto
     {
-        string _descricao;
+       [PrimaryKey, AutoIncrement]
 
-        [PrimaryKey, AutoIncrement]
+public int Id { get; set; }
 
-        public int Id {  get; set; }
+public string Descricao { get; set; }
+public double Preco { get; set; }
 
-        public string Descricao
-        {
-            get => _descricao;
-            set
-            {
-                if (value == null)
-                {
-                    throw new Exception(
-                        "Por favor, preencha a descrção");
-                }
-                _descricao = value;
-            }
-        }
-
-        public double Quantidade { get; set; }
-        public double Preco {  get; set; } 
-        public double Total { get => Quantidade * Preco; }
+public double Quantidade { get; set; }
+public double Total { get => Quantidade * Preco; }
 
         
         
